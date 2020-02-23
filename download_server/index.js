31236f459144b471018c485b66b029b1170c0e58
Sharmaprateek52 <prateekkr12@gmail.com>
2020-02-23 16:46:42 +0530

    Added a thankyou page in download_server

--- download_server/index.js
+++ download_server/index.js
@@ -2,15 +2,11 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 app.get('/',function(req,res){
-    res.sendFile('/home/prateek/Documents/nodejs/open_genus/Download_server/html_file.html');
+    res.sendFile('/home/prateek/Documents/nodejs/open_genus/download_server/html_file.html');
 })
 
 app.get('/download',function(req,res){
     res.download(__dirname +'/upload_folder/ichigo.jpeg','ichigo.jpeg');
-    console.log("File has been downloaded!");
-})
-app.get('/Thanks',function(req,res){
-    res.sendFile('/home/prateek/Documents/nodejs/open_genus/Download_server/thankyou.html')
 })
 
 http.listen(3000,function(){
