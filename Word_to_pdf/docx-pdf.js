d5ab113c4f6bafe358015be82fd2ad40d8f521a4
Sharmaprateek52 <prateekkr12@gmail.com>
2020-02-29 20:01:38 +0530

    Removed a directory

--- Word_to_pdf/docx-pdf.js
+++
@@ -1,8 +0,0 @@
-var docxConverter = require('docx-pdf');
-
-docxConverter('./test_file.docx','./output.pdf',function(err,result){
-  if(err){
-    console.log(err);
-  }
-  console.log('result'+result);
-});
